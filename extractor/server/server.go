44f1bab8ed3ee0fb2a657f59c2c63b4e127bcd31
Alexander Bezzubov <bzz@apache.org>
2017-07-12 16:56:36 +0200

    Add profiling suport to server

--- extractor/server/server.go
+++ extractor/server/server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
 	"net"
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/src-d/berserker/extractor"
 
@@ -9,11 +12,17 @@ import (
 	"google.golang.org/grpc"
 )
 
-func main() {
-	// TODO parametrize
-	url := "localhost:8888"
+var profiler = flag.Bool("profiler", false, "start CPU & memeory profiler")
 
-	lis, err := net.Listen("tcp", url)
+func main() {
+	flag.Parse()
+	// TODO parametrize
+	profilerAddr := "localhost:6062"
+	grpcAddr := "localhost:8888"
+
+	startHTTPProfilingMaybe(profilerAddr)
+
+	lis, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
 		panic(err)
 	}
@@ -21,10 +30,22 @@ func main() {
 	grpcServer := grpc.NewServer()
 
 	extractor.RegisterExtractorServiceServer(grpcServer, extractor.NewExtractorServiceServer())
-	log.Info("server started", "URL", url)
+	log.Info("server started", "address", grpcAddr)
 
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		log.Error("server error", "err", err)
+	}
+}
+
+func startHTTPProfilingMaybe(addr string) {
+	if *profiler {
+		go func() {
+			log.Debug("Started CPU & Heap profiler at", "address", addr)
+			err := http.ListenAndServe(addr, nil)
+			if err != nil {
+				log.Warn("Profiler failed to listen and serve", "address", addr, "err", err)
+			}
+		}()
 	}
 }
