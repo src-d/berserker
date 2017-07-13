3b40baf2b973e4caa25a4a973e0d5a2f59d21758
Antonio Jesus Navarro Perez <antnavper@gmail.com>
2017-07-13 15:08:09 +0200

    Analyzer: parametrize configuration params

--- normalizer/src/main/scala/tech/sourced/berserker/normalizer/Main.scala
+++ normalizer/src/main/scala/tech/sourced/berserker/normalizer/Main.scala
@@ -1,24 +1,36 @@
 package tech.sourced.berserker.normalizer
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import tech.sourced.berserker.normalizer.model.Schema
 import tech.sourced.berserker.normalizer.service.ExtractorService
 
+/**
+  * Start Berserker application.
+  *
+  * Some useful variables:
+  * - spark.sql.parquet.compression.codec: uncompressed, snappy, gzip, lzo
+  * - tech.sourced.berserker.grpc.host: grpc service hostname or ip, localhost
+  * by default
+  * - tech.sourced.berserker.grpc.port: grpc service port
+  * - tech.sourced.berserker.grpc.plaintext: if true, messages over grpc will
+  * be sent in plain text
+  * - tech.sourced.berserker.parquet.files.mode: parquet mode used to write
+  * the 'files' output
+  * - tech.sourced.berserker.parquet.files.name: parquet file name for 'files'
+  * output
+  */
 object Main extends App {
-  // TODO parametrize
-  var masterUrl = "local[2]"
-  var appName = "normalizer"
-  var grpcHost = "localhost"
-  var grpcPort = 8888
-  var grpcPlainText = true
-  var parquetCompression = "none"
-  var parquetMode = "overwrite"
-  var parquetFilename = "parquet"
+  var conf = new SparkConf()
+  conf.setAppName("berserker")
+  var grpcHost = conf.get("tech.sourced.berserker.grpc.host", "localhost")
+  var grpcPort = conf.getInt("tech.sourced.berserker.grpc.port", 8888)
+  var grpcPlainText = conf.getBoolean("tech.sourced.berserker.grpc.plaintext",
+    defaultValue = true)
+  var parquetMode = conf.get("tech.sourced.berserker.parquet.files.mode", "overwrite")
+  var parquetFilename = conf.get("tech.sourced.berserker.parquet.files.name", "files")
 
-  val spark = SparkSession.builder()
-    .appName(appName)
-    .master(masterUrl)
-    .getOrCreate()
+  val spark = SparkSession.builder().config(conf).getOrCreate()
 
   // Start gRPC connection
   val extractorService = ExtractorService(grpcHost, grpcPort, grpcPlainText)
@@ -26,7 +38,7 @@ object Main extends App {
   val dataRDD = spark.sparkContext.parallelize(extractorService.getRepositoriesData)
   val dataDF = spark.sqlContext.createDataFrame(dataRDD, Schema.files)
 
-  dataDF.write.option("compression", parquetCompression)
+  dataDF.write
     .mode(parquetMode)
     .parquet(parquetFilename)
   dataDF.show(10)
